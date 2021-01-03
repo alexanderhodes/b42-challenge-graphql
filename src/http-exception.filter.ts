25a628c3f85cd8f729e67df781d1d92e9e5d19ee
alexanderhodes <alexander.hodes@live.com>
2021-01-03 19:37:19 +0100

    updated exception filter just on error

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -5,10 +5,8 @@ export class HttpExceptionFilter implements ExceptionFilter {
     private readonly logger = new Logger(HttpExceptionFilter.name);
 
     catch(exception: any, host: ArgumentsHost): any {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
-        this.logger.error(exception);
+        if (exception) {
+            this.logger.error(exception);
+        }
     }
 }
