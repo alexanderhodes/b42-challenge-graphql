46d23d896ffc7252d0d4b6e3bb374cace755ed4d
alexanderhodes <alexander.hodes@live.com>
2021-01-03 12:48:30 +0100

    added logger

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -2,25 +2,27 @@ import {Resolver, Query, Mutation, Args} from '@nestjs/graphql';
 import {TodoService} from './todo.service';
 import {TodoType} from './todo.type';
 import {TodoInput} from './todo.input';
-import {HttpException, HttpStatus} from '@nestjs/common';
+import {HttpException, HttpStatus, Logger} from '@nestjs/common';
 
 @Resolver(() => TodoType)
 export class TodoResolver {
 
-    constructor(private todoService: TodoService) {
-    }
+    private readonly logger = new Logger(TodoResolver.name);
+
+    constructor(private todoService: TodoService) {}
 
     @Query(() => [TodoType])
     async allTodos(): Promise<TodoType[]> {
-        console.log('findAll');
+        this.logger.log('allTodos');
         return await this.todoService.findAll();
     }
 
     @Query(() => TodoType)
     async todoById(@Args('id') id: number): Promise<TodoType> {
-        console.log('todoById', id);
+        this.logger.log(`todoById ${id}`)
         const todo = await this.todoService.findById(id);
         if (todo) {
+            this.logger.log(JSON.stringify(todo));
             return todo;
         }
         throw new HttpException(`todo with id ${id} not found`, HttpStatus.NOT_FOUND);
@@ -28,28 +30,33 @@ export class TodoResolver {
 
     @Mutation(() => TodoType)
     async createTodo(@Args('todo') todo: TodoInput): Promise<TodoType> {
-        console.log('create todo');
+        this.logger.log('create todo');
+        this.logger.log(JSON.stringify(todo));
         return await this.todoService.create(todo);
     }
 
     @Mutation(() => TodoType)
     async updateTodo(@Args('id') id: number, @Args('todo') todo: TodoInput): Promise<TodoType> {
-        console.log('update todo', id);
+        this.logger.log(`update todo ${id}`);
         const updatedTodo = await this.todoService.update(id, todo);
         if (updatedTodo) {
+            this.logger.log(JSON.stringify(updatedTodo));
             return updatedTodo;
         }
+        this.logger.error(`update failed for todo with id ${todo.id} and title ${todo.title}`);
         throw new HttpException(`update failed for todo with id ${todo.id} and title ${todo.title}`, HttpStatus.BAD_REQUEST);
     }
 
     @Mutation(() => TodoType)
     async deleteTodo(@Args('id') id: number): Promise<TodoType> {
-        console.log('delete todo', id);
+        this.logger.log(`delete todo ${id}`);
         const deletedTodo = await this.todoService.delete(id);
         if (deletedTodo) {
+            this.logger.log(JSON.stringify(deletedTodo));
             return deletedTodo;
         }
-        throw new HttpException(`update failed for todo with id ${id}`, HttpStatus.BAD_REQUEST);
+        this.logger.error(`delete failed for todo with id ${id}`);
+        throw new HttpException(`delete failed for todo with id ${id}`, HttpStatus.BAD_REQUEST);
     }
 
 }
